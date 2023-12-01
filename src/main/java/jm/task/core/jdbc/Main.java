a58f5e04ea32bf5500ce19a09722f3d375346b09
Dmitriy <dadinoff1@gmail.com>
2023-12-01 14:23:50 +0300

    First commit

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserServiceImpl userService = new UserServiceImpl(new UserDaoJDBCImpl());
+
+        userService.createUsersTable();
+
+        userService.saveUser("Eric", "Cartman", (byte) 10);
+        userService.saveUser("Kyle", "Broflovski", (byte) 10);
+        userService.saveUser("Stan", "Marsh", (byte) 10);
+        userService.saveUser("Kenny", "McCormick", (byte) 10);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,27 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    //  Требуется заполнить данные вашей локальной БД
+    private static final String URL_KEY = "jdbc:postgresql://localhost:...";
+    private static final String USER_KEY = "...";
+    private static final String PASSWORD_KEY = "...";
+
+    public static Connection getConnection() throws SQLException {
+
+        Connection connection;
+        try{
+            connection  = DriverManager.getConnection(
+                    URL_KEY,
+                    USER_KEY,
+                    PASSWORD_KEY);
+            return  connection;
+        } catch (SQLException e){
+            throw new SQLException("Ошибка при установлении соединения с базой данных.", e);
+        }
+    }
+}
